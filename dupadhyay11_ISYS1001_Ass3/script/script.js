3ed88a62c499bfe6c8d1adedca5ec64d602c5ef8
Diggaj Upadhyay <dcozupadhyay@gmail.com>
2021-05-27 00:05:14 +0800

    changing colors/headers/new layout/minimal
    adding new stuff for the final assessment like forms/new layout/new tabs/changing the fonts

--- dupadhyay11_ISYS1001_Ass3/script/script.js
+++ dupadhyay11_ISYS1001_Ass3/script/script.js
@@ -4,3 +4,29 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 } 
+
+
+
+/* King's Newslatter Form with Validation */
+const submitBtn = document.getElementById('submit-btn');
+
+const validate = (e) => {
+  e.preventDefault();
+  const username = document.getElementById('username');
+  const emailAddress = document.getElementById('email-address');
+  if (username.value === "") {
+    alert("Please enter your username.");
+    username.focus();
+    return false;
+  }
+  if (emailAddress.value === "") {
+    alert("Please enter your email address.");
+    emailAddress.focus();
+    return false;
+  }
+  
+  return true;
+}
+
+submitBtn.addEventListener('click', validate);
+
