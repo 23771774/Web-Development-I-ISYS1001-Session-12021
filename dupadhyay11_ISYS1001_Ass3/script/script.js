b1ed66e0d3be6288e6177f92262ef62c04d9257a
Diggaj Upadhyay <dcozupadhyay@gmail.com>
2021-05-31 15:55:35 +0800

    Modified index, created form, webskills from scratch again, modified bunch of css and added bunch of javascript

--- dupadhyay11_ISYS1001_Ass3/script/script.js
+++ dupadhyay11_ISYS1001_Ass3/script/script.js
@@ -30,3 +30,48 @@ const validate = (e) => {
 
 submitBtn.addEventListener('click', validate);
 
+/*onload function to alert users in Form Page*/
+function myFunction() {
+  alert("Welcome to King's Music Store");
+}
+
+/* Do not submit unless all fields are filled */
+function validateForm() {
+  var x=document.forms["myform"]["fname"].value;  
+  if(x==null || x=="" )
+  {
+      alert("First Name should be filled out");
+      return false;
+  }
+
+  var y=document.forms["myform"]["lname"].value;
+  if(y==null || y=="")
+  {
+      alert("Last Name should be filled out");
+      return false;
+  }
+
+  var y=document.forms["myform"]["useremail"].value;
+  if(y==null || y=="")
+  {
+      alert("Email is mandatory");
+      return false;
+  }
+
+  var y=document.forms["myform"]["commentbox"].value;
+  if(y==null || y=="")
+  {
+      alert("Comment is mandatory");
+      return false;
+  }
+
+  else
+  {
+      return true;
+  }
+}
+
+/* reset form */
+function reset() {
+  document.getElementById("resetForm").reset();
+}
